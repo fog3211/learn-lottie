0afeee81f320604897ecdcea27362f3486ecd3c8
fog3211 <384977954@qq.com>
2019-04-16 20:26:40 +0800

    vue-lottie

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div v-for="(item, index) in list">
+      <Lottie :options="defaultOptions(index)" :height="400" :width="400" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Lottie from "vue-lottie";
+import data_list from "./data";
+
+export default {
+  name: "app",
+  components: {
+    Lottie
+  },
+  data() {
+    return {
+      list: data_list
+    };
+  },
+  methods: {
+    defaultOptions(index) {
+      return {
+        animationData: this.list[index]
+      };
+    }
+  }
+};
+</script>
+
+<style>
+#app > div {
+  display: inline-block;
+}
+</style>
---
+++ src/data/index.js
@@ -0,0 +1,8 @@
+import data1 from "./data1.json";
+import data2 from "./data2.json";
+import data3 from "./data3.json";
+
+const data_list = [
+    data1, data2, data3
+]
+export default data_list;
